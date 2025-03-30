d21f0dbd05e1026e68ce4f3d5061c770594f7d84
Amin Hashemi <aaminhashemi74@gmail.com>
2025-03-30 20:04:05 +0330

    mock CSS imports

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+// jest.config.js
+module.exports = {
+  moduleNameMapper: {
+    '\\.css$': 'identity-obj-proxy', // Mock CSS imports
+  },
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+};
