3cb2932e1264054c38b63e17c672ad8cae77c5bd
Amin Hashemi <aaminhashemi74@gmail.com>
2025-04-01 23:58:50 +0330

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,9 @@ module.exports = {
   moduleNameMapper: {
     '\\.css$': 'identity-obj-proxy', // Mock CSS imports
   },
+  testPathIgnorePatterns: [
+    "<rootDir>/puppeteer.test.js"
+  ],
   setupFilesAfterEnv: ["@testing-library/jest-dom"],
   testEnvironment: 'jest-environment-jsdom', // Specify the environment explicitly
 };
